9de15c0ef52d431ac8db024466081bfa291578f4
Dhruv Arora <dhruvarora.code@gmail.com>
2023-05-20 18:45:29 +0530

    imlemented func mobile nav

--- 09-Omnifood-Optimizations/script.js
+++ 09-Omnifood-Optimizations/script.js
@@ -70,3 +70,12 @@ checkFlexGap();
   }
 }
 */
+
+////////////////////////////////////////////////////
+// Making mobile navigation work:-
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
